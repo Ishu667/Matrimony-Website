3072ea00ff93bacb17a393bbe6f2a813c594e74c
Priyanka Priyadarshini Biswal <146961364+Ishu667@users.noreply.github.com>
2024-11-04 10:39:29 +0530

    Update models.py

--- Matrimony/matrimony_app/models.py
+++ Matrimony/matrimony_app/models.py
@@ -6,8 +6,7 @@ from django.contrib.auth.models import User
 class Contact(models.Model):
     name = models.CharField(max_length=25)
     email = models.EmailField()
-    subject = models.CharField(max_length=25)
-    message = models.TextField()
+    
     
     def __str__(self):
         return self.name
